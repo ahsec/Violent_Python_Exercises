cac9f969c1a7790e4b89b3df50f5db2d19537538
Angel Segura <ah.sec7@gmail.com>
2017-08-17 22:05:39 -0400

    Adding remarks to files being downloaded, and failed during download. Before: 	A file successfully downloaded  would print its name only After: 	A [+] marker is added to a successfully downloaded file and 	a [!] marker is added to a failed download.

--- Chapt_3/ImageDownloader.py
+++ Chapt_3/ImageDownloader.py
@@ -5,8 +5,6 @@ import argparse
 from os.path import basename
 from BeautifulSoup import BeautifulSoup
 
-#urlList = []
-
 class ImageWebsite(object):
     ''' Website that contains images to be downloaded.
     By calling the init method 
@@ -36,9 +34,6 @@ class ImageWebsite(object):
                         raise SystemExit(e)
             self.maxRecursionDepth = self.maxRecursionDepth - 1
 
-        # call METHOD DOWNLOAD IMAGES,
-        # CHECK LEVEL, IF NECESSARY, CALL DOWNLOAD IMAGES AGAIN
-
     def download_images(self):
         netloc = urlparse.urlsplit(self.rootUrl).netloc.split('.')
         if netloc[-2] + netloc[-1] != self.website:
@@ -50,7 +45,7 @@ class ImageWebsite(object):
             urlContent = urllib2.urlopen(req).read()
             self.urlList.append(self.rootUrl)
         except urllib2.HTTPError as http_err:
-            print http_err
+            print '    [!] Error:' + http_err
             return
 
         self.soup = BeautifulSoup(''.join(urlContent))
@@ -62,81 +57,17 @@ class ImageWebsite(object):
             imgUrl = self.rootUrl[ : self.rootUrl.find(".com") + 4] + imgUrl if (imgUrl[ : 4] != "http") else imgUrl
             # download only the proper image files
             if imgUrl.lower().endswith(img_types):
-#                imgUrl.lower().endswith('.jpg') or \
-#                imgUrl.lower().endswith('.gif') or \
-#                imgUrl.lower().endswith('.png') or \
-#                imgUrl.lower().endswith('.bmp'):
                 try:
                     req = urllib2.Request(imgUrl,headers=self.hdr)
                     imgData = urllib2.urlopen(req).read()
                     if len(imgData) >= self.minImageFileSize:
-                        print "    " + imgUrl
+                        print "    [+] Downloaded: " + imgUrl
                         fileName = basename(urlparse.urlsplit(imgUrl)[2])
                         output = open(os.path.join(self.downloadLocationPath, fileName),'wb')
                         output.write(imgData)
                         output.close()
                 except Exception, e:
-                    print str(e) + imgUrl
-
-"""
-# recursively download images starting from the root URL
-def downloadImages(url, level): # the root URL is level 0
-    # do not go to other websites
-    global website
-    netloc = urlparse.urlsplit(url).netloc.split('.')
-    if netloc[-2] + netloc[-1] != website:
-        return
-
-    global urlList
-    if url in urlList: # prevent using the same URL again
-        return
-
-    try:
-        urlContent = urllib2.urlopen(url).read()
-        urlList.append(url)
-        print url
-    except:
-        return
-
-    soup = BeautifulSoup(''.join(urlContent))
-    # find and download all images
-    imgTags = soup.findAll('img')
-    for imgTag in imgTags:
-        imgUrl = imgTag['src']
-        imgUrl = url[ : url.find(".com") + 4] + imgUrl if (imgUrl[ : 4] != "http") else imgUrl
-        # download only the proper image files
-        if imgUrl.lower().endswith('.jpeg') or \
-            imgUrl.lower().endswith('.jpg') or \
-            imgUrl.lower().endswith('.gif') or \
-            imgUrl.lower().endswith('.png') or \
-            imgUrl.lower().endswith('.bmp'):
-            try:
-                imgData = urllib2.urlopen(imgUrl).read()
-                global minImageFileSize
-                if len(imgData) >= minImageFileSize:
-                    print "    " + imgUrl
-                    fileName = basename(urlparse.urlsplit(imgUrl)[2])
-                    output = open(os.path.join(downloadLocationPath, fileName),'wb')
-                    output.write(imgData)
-                    output.close()
-            except Exception, e:
-                print str(e)
-                # pass
-    print
-    print
-
-    # if there are links on the webpage then recursively repeat
-    if level > 0:
-        linkTags = soup.findAll('a')
-        if len(linkTags) > 0:
-            for linkTag in linkTags:
-                try:
-                    linkUrl = linkTag['href']
-                    downloadImages(linkUrl, level - 1)
-                except Exception, e:
-                    print str(e)
-                    # pass
-"""
+                    print '    [!] Error: ' + str(e) + imgUrl
 
 def main():
     parser = argparse.ArgumentParser(description='''Downloads images from a
@@ -157,20 +88,6 @@ def main():
     netloc = urlparse.urlsplit(rootUrl).netloc.split('.')
     website = netloc[-2] + netloc[-1]
     images = ImageWebsite(rootUrl, maxRecursionDepth, minImageFileSize, downloadLocationPath, website)
-#    downloadImages(rootUrl, maxRecursionDepth, minImageFileSize, downloadLocationPath, website)
 
 if __name__ == '__main__':
     main()
-'''
-rootUrl = cla[1]
-maxRecursionDepth = int(cla[2])
-downloadLocationPath = cla[3] # absolute path
-if not os.path.isdir(downloadLocationPath):
-    print downloadLocationPath + " is not an existing directory!"
-    os._exit(2)
-
-minImageFileSize = long(cla[4]) # in bytes
-netloc = urlparse.urlsplit(rootUrl).netloc.split('.')
-website = netloc[-2] + netloc[-1]
-downloadImages(rootUrl, maxRecursionDepth)
-'''
